bc2ad81c0519210747ab297a8f30b0f14e204bfd
MuseGt <nemanja.stefanovic3105@gmail.com>
2024-01-07 15:20:30 +0100

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-deploy';
+  allButtonsClicked = false
+  clickCount = 0
+  showTries = false
+
+  btn0 = false
+  btn1 = false
+  btn2 = false
+  btn3 = false
+
+  buttonClicked(index: number): void{
+    this.clickCount++;
+    if(this.clickCount == 4)
+      this.allButtonsClicked = true
+
+    switch(index){
+      case 0:
+        this.btn0 = true;
+        break;
+      case 1:
+        this.btn1 = true;
+        break;
+      case 2:
+        this.btn2 = true;
+        break;
+      case 3:
+        this.btn3 = true;
+        break;
+    }
+  }
 }
